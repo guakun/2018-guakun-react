00a64970e2591b72b48f209286853cc06965a8f5
unknown <2306831967@qq.com>
2018-08-02 22:40:21 +0800

    第一次提交

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function sayHi() {
+  alert('Hi')
+}
+
+class App extends Component {
+  render() {
+    return (
+      // xml React 会把下面的变成 js
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React 也不过如此</h1>
+        </header>
+        <p className="App-intro">
+          编辑 <code>src/App.js</code> 就能改变页面展示
+          <button onClick={function() {alert(1)}}>点我</button>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const div = document.getElementById('guakun');
+ReactDOM.render(<App />, div);
+// 渲染
